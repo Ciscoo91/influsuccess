fb632274d08d3fd4bdb3b1997ad3df7918aeac64
Hrouch Mohammed <mohammed.hrouch@etudiant.univ-rennes1.fr>
2020-09-12 20:12:48 +0200

    MHRO: get insta profiles urls

--- src/main/java/com/ffu/service/Impl/InstaScrapper.java
+++ src/main/java/com/ffu/service/Impl/InstaScrapper.java
@@ -3,12 +3,52 @@ package com.ffu.service.Impl;
 import com.ffu.service.Scrapper.Scrapper;
 import com.ffu.service.dto.ScrapperRequestDTO;
 import com.ffu.service.dto.ScrapperResponseDTO;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.stereotype.Component;
 
+import javax.print.Doc;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 @Component
-public class InstaScrapper  extends Scrapper {
+public class InstaScrapper extends Scrapper {
+
+
+    private static final Pattern instaUrlPattern = Pattern.compile("^(http|https)://(www.)?instagram.com/");
+
     @Override
     public ScrapperResponseDTO scrape(ScrapperRequestDTO scrapperRequestDTO) {
-        return null;
+
+        // Store all  instagram profiles links corresponding to the category(ies)
+        HashSet<String> profileUrls = new HashSet<>();
+        for (int i =0; i<2; i++) {
+            Set<Document> sitesToVisit = this.googleLinksScrapper(" fitness instagram influencers", Integer.toString(i));
+
+            for(Document site: sitesToVisit) {
+                profileUrls.addAll(
+                    site.select("a[href]")
+                    .stream()
+                    .map(link -> link.attr("href"))
+                    .filter(link -> instaUrlPattern.matcher(link).find()
+                        && !link.contains("explorer")
+                        && !link.contains("/p/")
+                        && !link.contains("/d/")
+                    )
+                    .collect(Collectors.toSet())
+                );
+            }
+        }
+
+        return fillScrapperResponseDTODataFromProfil(profileUrls);
+    }
+
+    private ScrapperResponseDTO fillScrapperResponseDTODataFromProfil(HashSet<String> profileUrls) {
+        ScrapperResponseDTO scrapperResponseDTO = new ScrapperResponseDTO();
+
+        return scrapperResponseDTO;
     }
 }
--- src/main/java/com/ffu/service/Scrapper/Scrapper.java
+++ src/main/java/com/ffu/service/Scrapper/Scrapper.java
@@ -5,9 +5,10 @@ import com.ffu.service.dto.ScrapperResponseDTO;
 import com.ffu.web.rest.errors.ScrappingErrorException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public abstract class Scrapper {
 
@@ -19,9 +20,14 @@ public abstract class Scrapper {
      */
     public Document getDocumentFromUrl(String url, String[] requestParameters) {
         try {
-            return Jsoup.connect(url)
-                .data(requestParameters)
-                .get();
+            if (requestParameters != null){
+                return Jsoup.connect(url)
+                    .data(requestParameters)
+                    .get();
+            } else {
+                return Jsoup.connect(url)
+                    .get();
+            }
         } catch (IOException e) {
             throw new ScrappingErrorException(e.getMessage());
         }
@@ -33,11 +39,17 @@ public abstract class Scrapper {
      * @param pageStart
      * @return
      */
-    public Elements googleLinksScrapper(String term, String pageStart){
+    public Set<Document> googleLinksScrapper(String term, String pageStart){
 
         String[] requestParameters = {"q",term,"start",pageStart};
         Document document = this.getDocumentFromUrl("https://google.com/search", requestParameters);
-        return document.select("a[href]");
+
+        Set<Document> sitesToVisit =  document.select("a[href]")
+            .stream().map(link -> link.attr("href"))
+            .filter(href -> href.contains("influencers") && href.matches("^(http|https)://(.*)"))
+            .map(url -> this.getDocumentFromUrl(url, null))
+            .collect(Collectors.toSet());
+        return sitesToVisit;
     }
 
     public abstract ScrapperResponseDTO scrape(ScrapperRequestDTO scrapperRequestDTO);
