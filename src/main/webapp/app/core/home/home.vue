fe5692c1fd9b388ef21f61e531cbd588d72e9ef7
Ciscoo91 <franciscomambo91@gmail.com>
2020-11-20 10:48:32 +0100

    MFRA: changed homepage cards

--- src/main/webapp/app/core/home/home.vue
+++ src/main/webapp/app/core/home/home.vue
@@ -2,46 +2,71 @@
   <div class="home h-100">
     <div class="col-md-12 px-0">
       <div class="row banner w-100 mx-0 d-flex justify-content-center align-items-center h-50">
-            <div class="title mt-5 col-md-6 col-sm-12 d-flex flex-column align-items-center">
-              <h1 class="display-3 text-center font-weight-bold text-white" v-text="$t('home.title')">InfluSuccess!</h1>
-              <p class="lead text-white text-center" v-text="$t('home.subtitle')"></p>
+            <div class="title mt-0 col-md-6 col-sm-12 d-flex flex-column align-items-center">
+              <h1 class="display-3 text-center font-weight-bold text-primary" v-text="$t('home.title')">InfluSuccess!</h1>
+              <p class="lead text-info text-center" v-text="$t('home.subtitle')"></p>
             </div>
       </div>
-      <div class="container mt-4">
-        <div class="row d-flex justify-content-center">
-          <div class="col-md-4">
-            <div class="profile-card-6"><img src="http://envato.jayasankarkr.in/code/profile/assets/img/profile-6.jpg" class="img img-responsive">
-                <div class="profile-name">JOHN
-                    <br>DOE</div>
-                <div class="profile-position">Lorem Ipsum Donor</div>
-                <div class="profile-overview">
-                    <div class="profile-overview">
-                        <div class="row text-center">
-                            <div class="col-xs-4">
-                                <h3>1</h3>
-                                <p>Rank</p>
-                            </div>
-                            <div class="col-xs-4">
-                                <h3>50</h3>
-                                <p>Matches</p>
-                            </div>
-                            <div class="col-xs-4">
-                                <h3>35</h3>
-                                <p>Goals</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+      <div class="container my-5">
+        <div class="row d-flex flex-column align-items-center justify-content-center my-5">
+          <!-- <p class="lead text-center my-5">Ayez accès un grand nombre d'influenceurs pour des collaborations enrichissantes</p> -->
+          <div>
+            <b-card-group deck>
+              <b-card
+                header="featured"
+                header-tag="header"
+                footer="Card Footer"
+                footer-tag="footer"
+                title="Title"
+              >
+                <b-card-text>Header and footers using props.</b-card-text>
+                <b-button href="#" variant="primary">Go somewhere</b-button>
+              </b-card>
+
+              <b-card title="Title" header-tag="header" footer-tag="footer">
+                <template #header>
+                  <h6 class="mb-0">Header Slot</h6>
+                </template>
+                <b-card-text>Header and footers using slots.</b-card-text>
+                <b-button href="#" variant="primary">Go somewhere</b-button>
+                <template #footer>
+                  <em>Footer Slot</em>
+                </template>
+              </b-card>
+            </b-card-group>
+          </div>
+          <div class="row card-container my-5">
+            <!-- <div class="col-md-4">
+              <div class="profile-card-6"><img src="/content/images/homepage_influencer_1.jpeg" class="img img-responsive">
+                  <div class="profile-name">JOHN
+                      <br>DOE</div>
+                  <div class="profile-overview">
+                      <div class="profile-overview">
+                          <div class="row text-center d-flex justify-content-around">
+                              <div class="col-xs-4">
+                                  <h3>1</h3>
+                                  <p>Rank</p>
+                              </div>
+                              <div class="col-xs-4">
+                                  <h3>50</h3>
+                                  <p>Matches</p>
+                              </div>
+                              <div class="col-xs-4">
+                                  <h3>35</h3>
+                                  <p>Goals</p>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
             </div>
-            </div>
-              <div class="col-md-4">
-                <div class="profile-card-6"><img src="http://envato.jayasankarkr.in/code/profile/assets/img/profile-6.jpg" class="img img-responsive">
-                    <div class="profile-name">JOHN
-                        <br>DOE</div>
-                    <div class="profile-position">Lorem Ipsum Donor</div>
+            <div class="col-md-4">
+                <div class="profile-card-6"><img src="/content/images/homepage_influencer_2.jpeg" class="img img-responsive">
+                    <div class="profile-name">JANE
+                        <br>SMITH</div>
                     <div class="profile-overview">
                       <div class="profile-overview">
-                          <div class="row text-center">
+                          <div class="row text-center d-flex justify-content-around">
                               <div class="col-xs-4">
                                   <h3>1</h3>
                                   <p>Rank</p>
@@ -59,14 +84,14 @@
                   </div>
             </div>
             </div>
-              <div class="col-md-4">
-                <div class="profile-card-6"><img src="http://envato.jayasankarkr.in/code/profile/assets/img/profile-6.jpg" class="img img-responsive">
-                    <div class="profile-name">JOHN
-                        <br>DOE</div>
+            <div class="col-md-4 mb-5">
+                <div class="profile-card-6"><img src="/content/images/homepage_influencer_3.jpeg" class="img img-responsive">
+                    <div class="profile-name">SARA
+                        <br>FLYNN</div>
                     <div class="profile-position">Lorem Ipsum Donor</div>
                     <div class="profile-overview">
                         <div class="profile-overview">
-                            <div class="row text-center">
+                            <div class="row text-center d-flex justify-content-around">
                                 <div class="col-xs-4">
                                     <h3>1</h3>
                                     <p>Rank</p>
@@ -83,11 +108,12 @@
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> -->
+          </div>
         </div>
       </div>
       <div class="description-section-container container my-5">
-        <div class="description-section-campaign row d-flex justify-content-between" id="description-section-campaign">
+        <div class="description-section-campaign row d-flex flex-md-row flex-md-column justify-content-between" id="description-section-campaign">
             <img src="/content/images/homepage_social_medias.jpg" class="description_social_marketing col-md-6" alt="">
 
             <div class="content col-md-6">
@@ -127,21 +153,10 @@
 
 <style scoped>
 .banner {
-  min-height: 70vh;
+  min-height: 40vh;
   height: 70vh;
   position: relative;
-  background-image: url('/content/images/homepage-banner.jpg');
-  background-size: cover;
-}
-
-.banner:before {
-  content: '';
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.5);
+  background-color: #E8EAF6;
 }
 
 .title {
@@ -162,79 +177,6 @@
 
 .content{
   line-height:2em;
-}
-
-
-
-.profile-card-6 {
-    max-width: 300px;
-    background-color: #FFF;
-    border-radius: 5px;
-    box-shadow: 0px 0px 25px rgba(0, 0, 0, 0.1);
-    overflow: hidden;
-    position: relative;
-    margin: 10px auto;
-    cursor: pointer;
-}
-
-.profile-card-6 img {
-    transition: all 0.15s linear;
-}
-
-.profile-card-6 .profile-name {
-    position: absolute;
-    top: 10px;
-    left: 10px;
-    font-size: 25px;
-    font-weight: bold;
-    color: #FFF;
-    padding: 15px 20px;
-    background: linear-gradient(140deg, rgba(0, 0, 0, 0.4) 50%, rgba(255, 255, 0, 0) 50%);
-    transition: all 0.15s linear;
-}
-
-.profile-card-6 .profile-position {
-    position: absolute;
-    color: rgba(255, 255, 255, 0.4);
-    left: 30px;
-    top: 100px;
-    transition: all 0.15s linear;
-}
-
-.profile-card-6 .profile-overview {
-    position: absolute;
-    bottom: 0px;
-    left: 0px;
-    right: 0px;
-    background: linear-gradient(0deg, rgba(0, 0, 0, 0.4) 50%, rgba(255, 255, 0, 0));
-    color: #FFF;
-    padding: 50px 0px 20px 0px;
-    transition: all 0.15s linear;
-}
-
-.profile-card-6 .profile-overview h3 {
-    font-weight: bold;
-}
-
-.profile-card-6 .profile-overview p {
-    color: rgba(255, 255, 255, 0.7);
-}
-
-.profile-card-6:hover img {
-    filter: brightness(80%);
-}
-
-.profile-card-6:hover .profile-name {
-    padding-left: 25px;
-    padding-top: 20px;
-}
-
-.profile-card-6:hover .profile-position {
-    left: 40px;
-}
-
-.profile-card-6:hover .profile-overview {
-    padding-bottom: 25px;
 }
 
 </style>
