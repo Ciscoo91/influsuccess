9447dd4f4a28a307a03fd89bcdac8db974523282
Ciscoo91 <franciscomambo91@gmail.com>
2020-11-21 02:09:31 +0100

    MFRA: added card in position absolute + editing it's content

--- src/main/webapp/app/core/home/home.vue
+++ src/main/webapp/app/core/home/home.vue
@@ -1,120 +1,50 @@
 <template>
   <div class="home h-100">
-    <div class="col-md-12 px-0">
       <div class="row banner w-100 mx-0 d-flex justify-content-center align-items-center h-50">
             <div class="title mt-0 col-md-6 col-sm-12 d-flex flex-column align-items-center">
               <h1 class="display-3 text-center font-weight-bold text-primary" v-text="$t('home.title')">InfluSuccess!</h1>
               <p class="lead text-info text-center" v-text="$t('home.subtitle')"></p>
             </div>
       </div>
-      <div class="container my-5">
-        <div class="row d-flex flex-column align-items-center justify-content-center my-5">
-          <!-- <p class="lead text-center my-5">Ayez accès un grand nombre d'influenceurs pour des collaborations enrichissantes</p> -->
-          <div>
-            <b-card-group deck>
-              <b-card
-                header="featured"
-                header-tag="header"
-                footer="Card Footer"
-                footer-tag="footer"
-                title="Title"
+      <div class="card-section container mb-5 mt-n5">
+          <b-card-group deck class="card-group">
+            <b-card
+              img-src="https://via.placeholder.com/350x150/303f9f/FFFFFF"
+              class="home-card border-0 shadow-sm"
+              title="Influenceurs"
+              img-top
+              align="center"
               >
-                <b-card-text>Header and footers using props.</b-card-text>
-                <b-button href="#" variant="primary">Go somewhere</b-button>
-              </b-card>
+              <b-card-text>
+                Trouvez parmis des milliers de partenariats potentiels pour augmenter votre audiance et votre protefeuille
+              </b-card-text>
 
-              <b-card title="Title" header-tag="header" footer-tag="footer">
-                <template #header>
-                  <h6 class="mb-0">Header Slot</h6>
-                </template>
-                <b-card-text>Header and footers using slots.</b-card-text>
-                <b-button href="#" variant="primary">Go somewhere</b-button>
-                <template #footer>
-                  <em>Footer Slot</em>
-                </template>
-              </b-card>
-            </b-card-group>
-          </div>
-          <div class="row card-container my-5">
-            <!-- <div class="col-md-4">
-              <div class="profile-card-6"><img src="/content/images/homepage_influencer_1.jpeg" class="img img-responsive">
-                  <div class="profile-name">JOHN
-                      <br>DOE</div>
-                  <div class="profile-overview">
-                      <div class="profile-overview">
-                          <div class="row text-center d-flex justify-content-around">
-                              <div class="col-xs-4">
-                                  <h3>1</h3>
-                                  <p>Rank</p>
-                              </div>
-                              <div class="col-xs-4">
-                                  <h3>50</h3>
-                                  <p>Matches</p>
-                              </div>
-                              <div class="col-xs-4">
-                                  <h3>35</h3>
-                                  <p>Goals</p>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-            </div>
-            <div class="col-md-4">
-                <div class="profile-card-6"><img src="/content/images/homepage_influencer_2.jpeg" class="img img-responsive">
-                    <div class="profile-name">JANE
-                        <br>SMITH</div>
-                    <div class="profile-overview">
-                      <div class="profile-overview">
-                          <div class="row text-center d-flex justify-content-around">
-                              <div class="col-xs-4">
-                                  <h3>1</h3>
-                                  <p>Rank</p>
-                              </div>
-                              <div class="col-xs-4">
-                                  <h3>50</h3>
-                                  <p>Matches</p>
-                              </div>
-                              <div class="col-xs-4">
-                                  <h3>35</h3>
-                                  <p>Goals</p>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-            </div>
-            </div>
-            <div class="col-md-4 mb-5">
-                <div class="profile-card-6"><img src="/content/images/homepage_influencer_3.jpeg" class="img img-responsive">
-                    <div class="profile-name">SARA
-                        <br>FLYNN</div>
-                    <div class="profile-position">Lorem Ipsum Donor</div>
-                    <div class="profile-overview">
-                        <div class="profile-overview">
-                            <div class="row text-center d-flex justify-content-around">
-                                <div class="col-xs-4">
-                                    <h3>1</h3>
-                                    <p>Rank</p>
-                                </div>
-                                <div class="col-xs-4">
-                                    <h3>50</h3>
-                                    <p>Matches</p>
-                                </div>
-                                <div class="col-xs-4">
-                                    <h3>35</h3>
-                                    <p>Goals</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div> -->
-          </div>
-        </div>
+              <b-card-text>A second paragraph of text in the card.</b-card-text>
+
+              <a href="#" class="card-link">Card link</a>
+              <b-link href="#" class="card-link">Another link</b-link>
+            </b-card>
+            <b-card
+              img-src="https://via.placeholder.com/350x150/303f9f/FFFFFF"
+              class="home-card border-0 shadow-sm"
+              title="Annonceurs"
+              img-top
+              align="center"
+            >
+              <b-card-text>
+                Postez des annonces et collaborez avec un ou plusieurs influenceurs dans la liste des milliers d'influenceurs que contient notre plateforme
+              </b-card-text>
+
+              <b-card-text>A second paragraph of text in the card.</b-card-text>
+
+              <a href="#" class="card-link">Card link</a>
+              <b-link href="#" class="card-link">Another link</b-link>
+            </b-card>
+          </b-card-group>
       </div>
       <div class="description-section-container container my-5">
-        <div class="description-section-campaign row d-flex flex-md-row flex-md-column justify-content-between" id="description-section-campaign">
-            <img src="/content/images/homepage_social_medias.jpg" class="description_social_marketing col-md-6" alt="">
+        <div class="description-section-campaign row d-flex flex-md-row flex-sm-column justify-content-between align-items-sm-center" id="description-section-campaign">
+            <img src="/content/images/homepage_social_medias.jpg" class="description_social_image col-md-6 mx-sm-auto" alt="">
 
             <div class="content col-md-6">
               <h3>Entreprise, pourquoi investir dans le marketing d'influence ?</h3>
@@ -141,11 +71,10 @@
                 </ul>
               </div>
             </div>
-            <img src="/content/images/homepage_social_influencer.jpg" class="description_social_marketing col-md-6" alt="">
+            <img src="/content/images/homepage_social_influencer.jpg" class="description_social_image col-md-6" alt="">
         </div>
       </div>
     </div>
-  </div>
 </template>
 
 <script lang="ts" src="./home.component.ts">
@@ -171,8 +100,10 @@
   box-shadow: rgba(255, 255, 255, 0.7);
 }
 
-.description_social_marketing{
-  width: 50%;
+.description_social_image{
+  width: 100%;
+  height: auto;
+  margin: 0 auto;
 }
 
 .content{
